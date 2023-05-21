7403e242c488e125d893961a8cc23383bdc56c85
yuri-italo <yuri.italo94@gmail.com>
2023-05-21 17:38:20 -0300

    interfaces da camada de serviço

---
+++ src/main/kotlin/dev/yuri/credit/application/system/service/ICreditService.kt
@@ -0,0 +1,10 @@
+package dev.yuri.credit.application.system.service
+
+import dev.yuri.credit.application.system.entity.Credit
+import java.util.UUID
+
+interface ICreditService {
+    fun save(credit: Credit): Credit
+    fun findAllByCustomer(customerId: Long): List<Credit>
+    fun findByCreditCode(creditCode: UUID): Credit
+}
