eedb89ef69b1392423fc3ab9b85ac85332604a3a
yuri-italo <yuri.italo94@gmail.com>
2023-05-22 15:29:24 -0300

    endpoints CreditResource - (findAllByCreditCode)

--- src/main/kotlin/dev/yuri/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/dev/yuri/credit/application/system/controller/CreditResource.kt
@@ -1,10 +1,13 @@
 package dev.yuri.credit.application.system.controller
 
 import dev.yuri.credit.application.system.dto.CreditDto
+import dev.yuri.credit.application.system.dto.CreditViewList
 import dev.yuri.credit.application.system.service.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,5 +19,12 @@ class CreditResource(
     fun save(@RequestBody creditDto: CreditDto): String {
         val credit = this.creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
+
+    @GetMapping()
+    fun findAllByCustomerId(@RequestParam customerId: Long): List<CreditViewList> {
+        return this.creditService.findAllByCustomer(customerId).stream()
+                .map { credit -> CreditViewList(credit) }
+                .toList()
     }
 }
