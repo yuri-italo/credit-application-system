8ab859e0a225baeb1f08d6d4d91b57fde625436d
yuri-italo <yuri.italo94@gmail.com>
2023-05-22 19:21:27 -0300

    nome do request mapping de customers corrigido e mostrar sql formatado habilitado

--- src/main/kotlin/dev/yuri/credit/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/dev/yuri/credit/application/system/controller/CustomerResource.kt
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/costumers")
+@RequestMapping("/api/customers")
 class CustomerResource(
         private val customerService: CustomerService
 ) {
