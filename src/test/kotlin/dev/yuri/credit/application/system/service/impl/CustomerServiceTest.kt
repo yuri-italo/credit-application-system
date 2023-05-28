aae1956a31f4d69f8d55e5a73e6e7616bc2cc9f3
yuri-italo <yuri.italo94@gmail.com>
2023-05-28 18:58:17 -0300

    configuracao do profile de teste

---
+++ src/test/kotlin/dev/yuri/credit/application/system/service/impl/CustomerServiceTest.kt
@@ -0,0 +1,25 @@
+package dev.yuri.credit.application.system.service.impl
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
