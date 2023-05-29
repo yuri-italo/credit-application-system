cca32794c672e832d8f1bd44908eb07ed9a7c60e
yuri-italo <yuri.italo94@gmail.com>
2023-05-29 13:50:52 -0300

    integration test - CreditRepositoryTest configuration

---
+++ src/test/kotlin/dev/yuri/credit/application/system/repository/CreditRepositoryTest.kt
@@ -0,0 +1,16 @@
+package dev.yuri.credit.application.system.repository
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditRepositoryTest {
+    @Autowired lateinit var customerRepository: CustomerRepository
+    @Autowired lateinit var testEntityManager: TestEntityManager
+
+}
