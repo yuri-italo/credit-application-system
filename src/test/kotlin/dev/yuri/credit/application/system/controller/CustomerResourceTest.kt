0ed41dad3713ec4773b1e6697cf70c26846eedfe
yuri-italo <yuri.italo94@gmail.com>
2023-05-29 14:38:58 -0300

    integration test - CustomerResourceTest configuration

---
+++ src/test/kotlin/dev/yuri/credit/application/system/controller/CustomerResourceTest.kt
@@ -0,0 +1,31 @@
+package dev.yuri.credit.application.system.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import dev.yuri.credit.application.system.repository.CustomerRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ContextConfiguration
+class CustomerResourceTest {
+    @Autowired private lateinit var customerRepository: CustomerRepository
+    @Autowired private lateinit var mockMvc: MockMvc
+    @Autowired private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL = "/api/customers"
+    }
+
+    @BeforeEach fun setup() = customerRepository.deleteAll()
+    @AfterEach fun tearDown() = customerRepository.deleteAll()
+
+}
